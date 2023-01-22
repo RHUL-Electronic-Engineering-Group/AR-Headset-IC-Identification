7240fc816c1be1b1edafbf2dc4be08b73cf633e5
GeorgeSayles <georgesayles@gmail.com>
2023-01-22 17:33:59 +0000

    linking python scripts together
    with jake figured out how to call the lib_reader script from the OCR text search script so that we can automatically pass the text on the IC to the lib reader

--- IC_LIB_READER.py
+++ IC_LIB_READER.py
@@ -1,20 +1,26 @@
 from csv import DictReader
+import sys
 Found=False                                                                     #bool variable to check if component in library, default false
-IC_Name=input("Please enter the IC name: ")                                     #takes text on the IC as input name
-with open("IC_Library.csv", "r") as csv_file:                                   #open IC Library
-    IC_Lib=DictReader(csv_file)                                                 #uses csv reader to treat it as a dictionary
-    for row in IC_Lib:                                                          #cycles through all rows in the Library
-        if row['ï»¿name']==IC_Name:                                             #checks if the name matches the input
-            Found=True                                                          #sets found to true if the input matches a name in the library
-            print(row["ï»¿name"],':',row["description"])                        #outputs name and description
-            print(row["link to datasheet"])                                     #provides link to datasheet
-            pinout=row['pinout'].split("|")                                     #splits pinout in half using marker in the CSV
-            sub0_pinout=pinout[0].split(",")                                    #deliminates each half to individual list elements
-            sub1_pinout=pinout[1].split(",")
-            sub1_pinout.reverse()                                               #reverse the second half to match actual pinout style
-            length=(len(sub1_pinout)+len(sub1_pinout))/2                        #checks number of pins and divides by 2, ensures it is actually split in half
-            for i in range(int(length)):
-                if i==0: print(sub0_pinout[i],'\t|--o--|',sub1_pinout[i])       #prints first pinout with a marker at the top like a real IC
-                else:    print(sub0_pinout[i],'\t|-----|',sub1_pinout[i])       #prints the rest of the pinout as it would be for the IC
-if Found==False:                                                                #if found never gets set to true the component is not in the library so reports this to the user
-    print("no component Found")
+try:
+    Found_Data=sys.argv[1].replace('\n','').split(',') 
+except:
+    Found_Data=input("input data manually: ").replace('\n','').split(',')
+for text in Found_Data:
+    IC_Name=text #input("Please enter the IC name: ")                                 #takes text on the IC as input name
+    with open("IC_Library.csv", "r") as csv_file:                                   #open IC Library
+        IC_Lib=DictReader(csv_file)                                                 #uses csv reader to treat it as a dictionary
+        for row in IC_Lib:                                                          #cycles through all rows in the Library
+            if row['ï»¿name']==IC_Name:                                             #checks if the name matches the input
+                Found=True                                                          #sets found to true if the input matches a name in the library
+                print(row["ï»¿name"],':',row["description"])                        #outputs name and description
+                print(row["link to datasheet"])                                     #provides link to datasheet
+                pinout=row['pinout'].split("|")                                     #splits pinout in half using marker in the CSV
+                sub0_pinout=pinout[0].split(",")                                    #deliminates each half to individual list elements
+                sub1_pinout=pinout[1].split(",")
+                sub1_pinout.reverse()                                               #reverse the second half to match actual pinout style
+                length=(len(sub1_pinout)+len(sub1_pinout))/2                        #checks number of pins and divides by 2, ensures it is actually split in half
+                for i in range(int(length)):
+                    if i==0: print(sub0_pinout[i],'\t|--o--|',sub1_pinout[i])       #prints first pinout with a marker at the top like a real IC
+                    else:    print(sub0_pinout[i],'\t|-----|',sub1_pinout[i])       #prints the rest of the pinout as it would be for the IC
+    if Found==False:                                                                #if found never gets set to true the component is not in the library so reports this to the user
+        print("Text Not Found")
--- OcrTextSearch.py
+++ OcrTextSearch.py
@@ -1,48 +1,25 @@
+import subprocess
 try:
-     
-     # opening and reading the file 
-     file_read = open("gcloudRead.txt", "r")
-     
-     # asking the user to enter the string to be 
-     # searched
-     text = '"text":'
-     
-     # reading file content line by line.
-     lines = file_read.readlines()
-     
+     #subprocess.call(r'OCR.bat')                                               # runs the OCR.bat file, is commented out for testing purposes
+     file_read = open("gcloudRead.txt", "r")                                    # opening and reading the file 
+     text = '"text":'                                                           # asking the user to enter the string to be searched
+     lines = file_read.readlines()                                              # reading file content line by line.
      new_list = []
      idx = 0
-     
-     # looping through each line in the file
-     for line in lines:
-               
-          # if line have the input string, get the index 
-          # of that line and put the
-          # line into newly created list 
-          if text in line:
-               new_list.insert(idx, line)
+     for line in lines:                                                         # looping through each line in the file
+          if text in line:                                                      # if line have the input string, get the index of that 
+               new_list.insert(idx, line)                                            #line and put the line into newly created list 
                idx += 1
-     
-     # closing file after reading
-     file_read.close()
-     
-     # if length of new list is 0 that means 
-     # the input string doesn't
-     # found in the text file
-     if len(new_list)==0:
+          file_read.close()                                                     # closing file after reading
+     if len(new_list)==0:                                                       # if length of new list is 0 that means the input string doesn't found in the text file
           print("\n\"" +text+ "\" is not found in \"" +file_name+ "\"!")
      else:
-     
-          # displaying the lines 
-          # containing given string
-          foundText = new_list[len(new_list)-1]
+          foundText = new_list[len(new_list)-1]                                 # displaying the lines containing given string
           foundText = foundText[17:]
           foundText = foundText.replace('\\n',',')
           foundText = foundText.replace('"','')
-
           print(foundText)
+          subprocess.run(['python','IC_LIB_READER.py',foundText])    
 
-# entering except block
-# if input file doesn't exist 
-except :
+except :                                                                        # entering except block if input file doesn't exist 
     print("\nThe file doesn't exist!")

132388a3750da2ac267cb41e7ba6de230067f095
JasonMaher23 <57668903+JasonMaher23@users.noreply.github.com>
2023-01-26 21:11:38 +0000

    Async web server as intermediary
    will need to add both the AsyncTCP library and ESPAsyncWebServe library for the photoServer to function. This can be added in the arduino program under Sketch > Include Library > Add .ZIP library…

--- Working/jpegGrabber.py
+++ Working/jpegGrabber.py
@@ -1,8 +1,8 @@
 import requests # request img from web
 from PIL import Image
 
-url = "https://i.imgur.com/ke78nj5.jpeg"
-filename = "cat.jpeg"
+url = "http://192.168.1.64/saved-photo"
+filename = "Snapshot.jpeg"
 
 im = Image.open(requests.get(url, stream=True).raw)             #pull image from url provided
 im = im.save(filename)                                          #save image

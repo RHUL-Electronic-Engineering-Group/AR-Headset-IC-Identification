12ec2c590206c74d55fdd02e070b5475a8600f9d
JasonMaher23 <57668903+JasonMaher23@users.noreply.github.com>
2023-02-23 00:14:41 +0000

    camera web server

--- Working/jpegGrabber.py
+++ Working/jpegGrabber.py
@@ -1,7 +1,7 @@
 import requests # request img from web
 from PIL import Image
 
-url = "http://192.168.1.64/saved-photo"
+url = "http://192.168.1.66/capture"
 filename = "Snapshot.jpeg"
 
 im = Image.open(requests.get(url, stream=True).raw)             #pull image from url provided

5932893ecf98e5f63b243558c05e2c042d5ecd37
Jake Cornwell <zfee003@live.rhul.ac.uk>
2023-03-21 20:24:33 +0000

    Updates for ESP two way

--- ESP32/TFT_Test/TFT_test/TFT_test.ino
+++ ESP32/TFT_Test/TFT_test/TFT_test.ino
@@ -3,71 +3,86 @@
 #include <Adafruit_I2CDevice.h>
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>             // Arduino SPI library
+#include "EspMQTTClient.h"
 
-#define TFT_MOSI 23  // SDA Pin on ESP32
-#define TFT_SCLK 18  // SCL Pin on ESP32
-#define TFT_CS   15  // Chip select control pin
-#define TFT_DC    2  // Data Command control pin
-#define TFT_RST   4  // Reset pin (could connect to RST pin)
+#define TFT_MOSI 14  // SDA Pin on ESP32
+#define TFT_SCLK 25  // SCL Pin on ESP32
+#define TFT_CS   26  // Chip select control pin
+#define TFT_DC   33  // Data Command control pin
+#define TFT_RST  27 // Reset pin (could connect to RST pin)
 
+EspMQTTClient client(
+  "JasonHotspot",
+  "jason12345",
+  "test.mosquitto.org",  // MQTT Broker server ip
+  "",   // Can be omitted if not needed
+  "",   // Can be omitted if not needed
+  "ESP32-Headset-Demo-Immersive",     // Client name that uniquely identify your device
+  1883              // The MQTT port, default to 1883. this line can be omitted
+);
 
 // Initialize Adafruit ST7789 TFT library
-Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details. 
+//Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_PORT, MQTT_USERNAME, MQTT_PASSWORD); 
+/****************************** Feeds ***************************************/ 
+// Code for LCD code
  
 float p = 3.1415926;
  
-void tftPrintTest() {
+//void MQTT_connect(); 
+void setup(void) {
+
+  client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
+  client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overridded with enableHTTPWebUpdater("user", "password").
+  client.enableOTA(); // Enable OTA (Over The Air) updates. Password defaults to MQTTPassword. Port is the default OTA port. Can be overridden with enableOTA("password", port).
+  client.enableLastWillMessage("TestClient/lastwill", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
+
   tft.setTextWrap(false);
   tft.fillScreen(ST77XX_BLACK);
   tft.setCursor(0, 30);
   tft.setTextColor(ST77XX_RED);
   tft.setTextSize(1);
-  tft.println("Test");
-  tft.setTextColor(ST77XX_YELLOW);
-  tft.setTextSize(2);
-  tft.println("Test");
-  tft.setTextColor(ST77XX_GREEN);
-  tft.setTextSize(3);
-  tft.println("Test");
-  tft.setTextColor(ST77XX_BLUE);
-  tft.setTextSize(4);
-  tft.print(1234.567);
-  delay(1500);
-  tft.setCursor(0, 0);
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(0);
-  tft.println("Test");
-  tft.setTextSize(1);
-  tft.setTextColor(ST77XX_GREEN);
-  tft.print(p, 6);
-  tft.println(" Test");
-  tft.println(" ");
-  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
-  tft.println(" Print HEX!");
-  tft.println(" ");
-  tft.setTextColor(ST77XX_WHITE);
-  tft.println("Sketch has been");
-  tft.println("running for: ");
-  tft.setTextColor(ST77XX_MAGENTA);
-  tft.print(millis() / 1000);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.print(" seconds.");
-}
- 
-void setup(void) {
+  
   Serial.begin(115200);
-    
+  pinMode(21, OUTPUT);
+  pinMode(19, OUTPUT);
+  pinMode(18, OUTPUT);
+  digitalWrite(21, HIGH);
+  digitalWrite(19, HIGH);
+  digitalWrite(18, LOW);
+  tft.setRotation(2);
   tft.init(240, 240, SPI_MODE2);    // Init ST7789 display 240x240 pixel
-  tft.setRotation(3);
   tft.fillScreen(ST77XX_BLACK);
+}
+
+// This function is called once everything is connected (Wifi and MQTT)
+// WARNING : YOU MUST IMPLEMENT IT IF YOU USE EspMQTTClient
+void onConnectionEstablished()
+{
+
+  // Subscribe to "mytopic/test" and display received message to Serial
+  // client.subscribe("immersive_headset_demo", [](const String & payload) {
+  // Serial.println(payload);
+   
+  // 
+  // ;
+
+  // });
+
+  client.subscribe("immersive_headset_demo", [](const String & topic, const String & payload) {
+   Serial.println(payload);
+   
+  });
+
+  // Execute delayed instructions
+ // client.executeDelayed(5 * 1000, []() {
+ //   client.publish("immersive_headset_demo/testpublish", "Maintaining Connection");
+ // });
 }
  
 void loop() {
-  tft.invertDisplay(true);
-  tftPrintTest();
-  delay(1000);
-  tft.invertDisplay(false);
-  tftPrintTest();
-  delay(1000);
+  client.loop();
+ 
 }
